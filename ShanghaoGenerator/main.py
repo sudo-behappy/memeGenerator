b7dc7e1c20b4ba549516060df768697424cf85af
sudo-behappy <3216539984@QQ.com>
2022-01-30 09:53:40 +0800

    添加readme.md, 优化文件架构, 添加manual

--- ShanghaoGenerator/main.py
+++ ShanghaoGenerator/main.py
@@ -1,9 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
+import os
 # 变量输入
-BG = Image.open('shanghao_empty.png')
+root = os.path.dirname(os.path.realpath(__file__))
+BG = Image.open(root + '\\res\\shanghao_empty.png')
 fontSize = 30
+
 try:
-    ico = Image.open('icon.png')
+    ico = Image.open(root + '\\icon.png')
 except FileNotFoundError:
     exit("please put icon under the program directory and name it \"icon.png\".")
 title = input('What do you want for title: ')
@@ -16,11 +19,11 @@ ico = ico.resize((280, 280))
 # title生成图片
 t = Image.new(mode = 'RGBA', size = (int(len(title) * 18 * fontSize / 30), fontSize + 0), color = 'white')
 titleImage = ImageDraw.Draw(t)
-font = ImageFont.truetype(font = 'font.ttf', size = fontSize)
+font = ImageFont.truetype(font = root + '\\res\\font.ttf', size = fontSize)
 titleImage.text(xy = (0, 0), text = title, fill = (0, 0, 0), font = font)
 t.show()
 # 拼合图片
 BG.paste(ico, (500, 50))
 BG.paste(t, ((500 + int(abs((280 - t.size[0])) / 2)), 330))
-BG.save('./out/shanghao_' + title + '.png')
-Image.open('./out/shanghao_' + title + '.png').show()
+BG.save(root + './out/shanghao_' + title + '.png')
+Image.open(root + '\\out\\shanghao_' + title + '.png').show()
---
+++ ShanghaoGenerator/manual.md
@@ -0,0 +1,12 @@
+基于pillow开发
+
+将你想要上的号的icon放在根目录下, 命名为icon.png
+
+命令行运行
+```
+python ./main.py
+```
+输出在`./out`目录
+
+效果:
+![上号Blender](ShanghaoGenerator\out\shanghao_Blender.png)
